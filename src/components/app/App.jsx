fe3e30000db9e7e9405bf54741032a959552e452
laamora <laamora@yandex.ru>
2021-10-15 00:18:13 +0700

    add comment to src/components/app/App.jsx

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -5,7 +5,7 @@ import { data } from "../../utils/data";
 import { dataForConstructor } from "../../utils/dataForConstructor";
 import BurgerIngedients from "../burger-ingredients/burger-ingredients";
 import BurgerConstructor from "../burger-constructor/burger-constructor";
-// хочу предупредить, что у меня экран 11 дюймов, поэтому ширина и высота главного компонента могут отличаться от макета
+// хочу предупредить, что у меня экран 13 дюймов, поэтому ширина и высота главного компонента могут отличаться от макета
 // вы можете посмотреть как это выглядит на моем ноуте в src/images/Снимок экрана 2021-10-14 в 23.30.24.png
 
 function App() {
